8c6d8b9947d58bd92bd7dd662807b0eb528c7563
Robby Workman <rworkman@slackware.com>
2018-01-06 17:33:02 -0600

    post-functions.sh: check for /etc/lilo.conf too for lilo offer
    Existence of /etc/lilo.conf coupled with executable /sbin/lilo
    probably indicates lilo usage on the system, so that seems like
    a reasonable compromise for everyone fussing about slackpkg's
    offer to run /sbin/lilo for them after kernel upgrades even
    though they use grub or whatever else.

--- files/post-functions.sh
+++ files/post-functions.sh
@@ -218,7 +218,7 @@ What do you want (K/O/R/P)?"
 lookkernel() {
 	NEWKERNELMD5=$(md5sum /boot/vmlinuz 2>/dev/null)
 	if [ "$KERNELMD5" != "$NEWKERNELMD5" ]; then
-		if [ -x /sbin/lilo ]; then
+		if [ -x /sbin/lilo ] && [ -r /etc/lilo.conf ]; then
 			echo -e "\n
 Your kernel image was updated.  We highly recommend you run: lilo
 Do you want slackpkg to run lilo now? (Y/n)"
@@ -228,9 +228,9 @@ Do you want slackpkg to run lilo now? (Y/n)"
 			fi
 		else
 			echo -e "\n
-Your kernel image was updated and lilo is not found on your system.
-You may need to adjust your boot manager (like GRUB) to boot appropriate
-kernel."
+Your kernel image was updated, and lilo does not appear to be used on 
+your system.  You may need to adjust your boot manager (like GRUB) to 
+boot the appropriate kernel."
 		fi
 	fi
 }
